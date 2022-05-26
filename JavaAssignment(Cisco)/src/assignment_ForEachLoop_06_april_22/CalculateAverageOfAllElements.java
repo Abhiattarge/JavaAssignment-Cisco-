5d318bbbdd0c1a24cc742a2f899876dbf3136143
Abhiattarge <attargeabhi@gmail.com>
2022-05-26 12:49:21 +0530

    Added For-Each-Loop Assignment

---
+++ JavaAssignment(Cisco)/src/assignment_ForEachLoop_06_april_22/CalculateAverageOfAllElements.java
@@ -0,0 +1,31 @@
+package assignment_ForEachLoop_06_april_22;
+
+import java.util.Scanner;
+
+public class CalculateAverageOfAllElements {
+
+	public static void main(String[] args) {
+		System.out.println("How many numbers you want to enter?");
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		double[] arr=new double[n];
+		double total=0;
+		
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("Enter Element no."+(i+1)+": ");
+			arr[i]=sc.nextDouble();
+		}
+		sc.close();
+		for(int i=0;i<arr.length;i++)
+		{
+			total = total + arr[i];
+		}
+		double average = total/arr.length;
+		
+		System.out.println("the average is:"+average);
+	
+	}
+
+}
---
+++ JavaAssignment(Cisco)/src/assignment_ForEachLoop_06_april_22/SquareOfAllElementArray.java
@@ -0,0 +1,24 @@
+package assignment_ForEachLoop_06_april_22;
+
+public class SquareOfAllElementArray {
+
+	public static void main(String[] args) {
+		int[] temp = {23,43,25,49,12,9,78,66,39};
+		
+		for(int value : temp)
+		{
+			System.out.println(value);
+		}
+		
+		for(int i=0;i<temp.length;i++)
+		{
+			temp[i]=(int)Math.pow(temp[i],2);
+		}
+		
+		for(int value : temp)
+		{
+			System.out.println(value);
+		}
+	}
+
+}
