4929a2c74de40f2c4778f1e8f82fc55e4ea4ffe6
Abhiattarge <attargeabhi@gmail.com>
2022-05-26 14:36:00 +0530

    Added LinkedHashSet And Tree Set Assignment

---
+++ JavaAssignment(Cisco)/src/linkedHashSetAndTreeSetAssignment/Employee.java
@@ -0,0 +1,16 @@
+
+package linkedHashSetAndTreeSetAssignment;
+
+public class Employee {
+	int empId;
+	String name;
+	float sal;
+	Employee(int empId, String name, float sal){
+		this.empId = empId;
+		this.name = name;
+		this.sal = sal;
+	}
+	public String toString() {
+		return "[ empId : "+ empId + ", name : "+ name +", sal : "+ sal+" ]";
+	}
+}
